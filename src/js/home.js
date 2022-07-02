693e25fb07286dbefb9a4f251c5d2d59a8ffb8e4
Taras Shapovalov <tarasshapovalov85@gmail.com>
2022-07-02 17:51:30 +0300

    add pagination and pagination on home.js

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,5 @@
 import NewsApiServise from './api-service';
-
+import handlerPagination from './pagination';
 const newsApiServise = new NewsApiServise();
 const movies = document.querySelector('.movies-home');
 const spinner = document.querySelector('.sk-circle');
@@ -9,7 +9,20 @@ function renderTrendMovies() {
   spinner.classList.remove('visually-hidden');
   newsApiServise.getTrendMovies().then(response => {
     console.log(response.results);
-
+const totalResult = response.total_results;
+    let currentPage = response.page;
+    
+    const instance = handlerPagination();
+          instance.setItemsPerPage(20);
+    instance.setTotalItems(totalResult);
+    instance.movePageTo(currentPage);
+    
+    instance.on('afterMove', event => {
+            newsApiServise.page = event.page;
+      const currentPage = newsApiServise.page;
+            newPage( currentPage);
+          });
+          this.page = this.page + 1;
     const markup = response.results
       .map(({ poster_path, original_title, release_date, genre_ids, vote_average }) => {
         return `<div class="movie-card">
@@ -49,4 +62,23 @@ getGenre()
 function toggleModal(e) {
     e.preventDefault();
     refs.modal.classList.toggle('is-hidden');
+}
+  
+function newPage(page) {
+  newsApiServise.getTrendMovies( page).then(response => {
+  const markup = response.results
+    .map(({ poster_path, original_title, release_date, genre_ids, vote_average }) => {
+      return `<div class="movie-card">
+                 <img class="movie-img" src="https://image.tmdb.org/t/p/w500${poster_path}" alt="card">
+            
+                 <div class="movie-info">
+                     <h2 class="movie-title">${original_title}</h2>
+                    <h3 class="span-title">${genre_ids} <span class="span-bord">${release_date}</span><span class="span-rejt">${(vote_average).toFixed(1)}</span></h3>
+                     </div>
+                 </div>`;
+    })
+    .join('');
+  movies.innerHTML = markup;
+  spinner.classList.add('visually-hidden');
+})
   }
